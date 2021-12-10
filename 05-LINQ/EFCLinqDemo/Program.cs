b8feb23d79e0d55d11da4ead9a8814fdaeb0bf17
PetkoGenchev <petko.genchev@yahoo.com>
2021-12-10 12:57:47 +0200

    EFC-07

---
+++ 05-LINQ/EFCLinqDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using EFCLinqDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EFCLinqDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new MusicXContext();
+
+            var songs = db.Songs.Where(x => x.Name.StartsWith("H")).ToList();
+
+            Console.WriteLine(songs.Count());
+        }
+    }
+}
