ad2bf75f0da61c41b8db2400da444110974fc9c2
PetkoGenchev <petko.genchev@yahoo.com>
2022-06-28 01:04:04 +0300

    FIxing Imports

---
+++ 06-Additions/XML/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace XML
+{
+
+    public class doc
+    {
+        public string @abstract { get; set; }
+
+        public string title { get; set; }
+    }
+
+    public class Program
+    {
+    
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = Encoding.Unicode;
+        }
+    }
+}
